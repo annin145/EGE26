a29c35556631452a503a759e9c0e3df2cfb9f4b8
annindanx <dementevaannet@gmil.com>
2025-08-07 18:59:06 +0200

    таблица преобразования типов данных

---
+++ pythonBase/lesson-2.py
@@ -0,0 +1,9 @@
+# таблица преобразования типов данных
+# int -> float(), str(), bool()
+# float -> int(), str(), bool()
+# str -> list(), tuple(), set(), bool()
+# list -> list(), tuple(), set(), bool()
+# tuple -> list(), str(), set(), bool()
+# set -> str(), list(),  tuple(), bool()
+# dict -> str(), list(),tuple(), set(), bool()
+# bool -> int(), float(), str()
