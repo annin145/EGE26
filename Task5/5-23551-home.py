ca98dd8208fc42f0a76af7268f7b75fd62caa629
annin145 <dementevaannet@gmail.com>
2025-11-09 18:05:21 +0200

    дз к 10.11 (5 задания) https://kompege.ru/variant?kim=25123353

--- Task5/5-23551-home.py
+++ Task5/5-23551-home.py
@@ -1,12 +1,11 @@
 for N in range(1,100_000):
-    R = bin(N)[2::]
-    if R.count('1') % 2 == 0:
-        R = '10' + R
-    else:
-        R = '1' + R + '01'
-    R = int(R, 2)
-    if R < 30:
-        print(N)
-        break
+     R = bin(N)[2::]
+     if R.count('1') % 2 == 0:
+         R = '10' + R
+     else:
+         R = '1' + R + '01'
+     R = int(R, 2)
+     if R < 30:
+         print(N)
 
-# N = 1
+# N = 6
