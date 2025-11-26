2f3e7c32822a9a0e75cc873d70d65eea0b7b89ed
annin145 <dementevaannet@gmail.com>
2025-11-26 22:02:16 +0200

    дз к 27.11 (5 задания) https://kompege.ru/variant?kim=25128914

--- Task5/5-21261-home.py
+++ Task5/5-21261-home.py
@@ -5,23 +5,17 @@ def convert(num, sys):
         num //= sys
     return res[::-1]
 
-ans = []
 for N in range(1,100_000):
     R = convert(N, 4)
     if sum(map(int, R)) % 3 == 0:
-        R.replace('2', '*')
-        R.replace('0', '2')
-        R.replace('*', '0')
-        R = R + '32'
+        R = R.replace('2', '*')
+        R = R.replace('0', '2')
+        R = R.replace('*', '0')
+        R ='32' + R
     else:
         R = R + '33'
-        r = R[2:3]
-        r.replace(r , '1')
-        n = R[3:4]
-        n.replace(n, '0')
-        #R = R[:1] + r + n + R[3:]
-    R = int(R,4)
+        R = R[0] + '10' + R[3:]
+    R = int(R, 4)
     if R == 335:
         print(N)
 
-#20
