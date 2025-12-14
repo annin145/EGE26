6b70a5b840b0e65674385495329b30d81367bcde
annin145 <dementevaannet@gmail.com>
2025-12-14 15:35:56 +0200

    дз к 15.12 https://kompege.ru/variant?kim=25134800

--- pythonBase/Task 8/8-5689-home.py
+++ pythonBase/Task 8/8-5689-home.py
@@ -5,7 +5,9 @@ alph = '01'
 cnt = 0
 for val in product(alph, repeat=16):
     val = ''.join(val)
-    if val[0] != 0:
+    if val[0] != '0':
         if val.count('1') % 3 == 0:
             cnt += 1
 print(cnt)
+
+#10923
