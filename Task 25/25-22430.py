993ee4eb0837d8421db416d4bcb556a02ea5f1d2
annindanx <dementevaannet@gmil.com>
2026-02-19 19:36:28 +0200

    ;

---
+++ Task 25/25-22430.py
@@ -0,0 +1,27 @@
+def is_prime(num):
+    if num < 2: return False
+    for i in range(2,int(num**.5)+1):
+        if num % i == 0:
+            return False
+    return True
+
+def f(num):
+    d = set()
+    for i in range(2,int(num**.5)+1):
+        if num % i == 0:
+            if is_prime(i): d |= {i}
+            if is_prime(num//i): d |= {num//i}
+    if len(d) >= 4:
+        d = sorted(d)
+        M = d[0]+d[1]+d[-2]+d[-1]
+        if M % 114 == 39:
+            return M
+    return 0
+
+cnt = 0
+for i in range(456790,10**30):
+    if M := f(i):
+        print(i,M)
+        cnt += 1
+        if cnt == 5:
+            break
