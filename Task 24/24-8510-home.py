ed48c523bc916b05b1dbf8b224a4099046a69977
annindanx <dementevaannet@gmil.com>
2026-03-23 18:50:13 +0200

    большая домашка

--- Task 24/24-8510-home.py
+++ Task 24/24-8510-home.py
@@ -1,13 +1,11 @@
 with open(r'.\file\24_8510.txt') as file:
     data = file.readline()
 
-for i in data:
-    if i in 'NO':
-        data = data.replace(i,'N')
+for i in 'PO':
+    data = data.replace(i,'N')
 
-for i in data:
-    if i in 'NN':
-        data = data.replace('NN','N N')
+while 'NN' in data:
+    data = data.replace('NN','N N')
 
 data = data.split()
 
