c79fce487771b2e518744a424e276569acfef3c9
annindanx <dementevaannet@gmil.com>
2026-03-23 19:57:52 +0200

    большая домашка

--- Task5/5-21243-home.py
+++ Task5/5-21243-home.py
@@ -11,13 +11,13 @@ for N in range(1,100_000):
     if sum(map(int,R)) % 5 == 0:
         R = R.replace('1', '*')
         R = R.replace('0', '1')
-        R = R.replace('*', '1')
+        R = R.replace('*', '0')
         R = R + '14'
     else:
         R = '44' + R[2:] + '33'
     R = int(R,5)
     if R > 370:
-        ans. append([R,N])
+        ans.append([R,N])
 
 print(min(ans))
 
