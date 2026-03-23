a601dc7d9ec84f96a7200e714aec44a80b2c978e
annindanx <dementevaannet@gmil.com>
2026-03-23 18:43:19 +0200

    большая домашка

--- Task 15/15-3893-2-home.py
+++ Task 15/15-3893-2-home.py
@@ -2,11 +2,11 @@ def d(x,a):
     return x % a == 0
 
 def f(x):
-    return d(abs(a),25) and (d(abs(x),24) and d(abs(x),75) <= d(abs(x),a))
+    return d(a,25) and ((d(x,24) and d(x,75)) <= d(x,a))
 
 cnt =0
 for a in range(-1000,1000):
-    if all(f(x) for x in range(-1000,1000)):
+    if a and all(f(x) for x in range(-1000,1000)):
         cnt += 1
 
 print(cnt)
