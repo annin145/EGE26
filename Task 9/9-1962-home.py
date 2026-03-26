94f1c785009ef4f6c7d762ae9b14c056d9a04187
annindanx <dementevaannet@gmil.com>
2026-03-26 18:09:32 +0200

    9 задачи кодом

--- Task 9/9-1962-home.py
+++ Task 9/9-1962-home.py
@@ -2,10 +2,8 @@ with open(r'.\files\1962.txt') as file:
     data = [list(map(int, i.split())) for i in file]
 
 cnt = 0
-for line in data:
-    for i in range(len(line)+1):
-        a,b,c = data[i:i+3]
-        if a+b > c and b+c > a and c+a > b:
-            cnt += 1
+for a,b,c  in data:
+    if a+b > c and b+c > a and c+a > b:
+        cnt += 1
 print(cnt)
 
