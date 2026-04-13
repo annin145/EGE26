b730fccb6f51896982596b26888c9050cba7995e
annindanx <dementevaannet@gmil.com>
2026-04-13 19:38:23 +0200

    13 задание

--- Task 13/13-14650.py
+++ Task 13/13-14650.py
@@ -3,7 +3,8 @@ from ipaddress import *
 ip1 = ip_address('216.54.187.235')
 ip2 = ip_address('216.54.174.128')
 
-for mask in range(16,25):
-    net = ip_network(f'{ip1}/{mask}', False)
-    if ip1 in net.hosts() and ip2 not in net.hosts():
+for mask in range(16,31):
+    net1 = ip_network(f'{ip1}/{mask}', False)
+    net2 = ip_network(f'{ip2}/{mask}', False)
+    if net1[0] != net2[0] and ip1 in net1.hosts() and ip2 in net2.hosts():
         print(mask)
