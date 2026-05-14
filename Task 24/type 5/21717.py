db9b75504fa3140b4670c4ea5ea520b74dc4afd2
annindanx <dementevaannet@gmil.com>
2026-05-14 19:39:06 +0200

    24

---
+++ Task 24/type 5/21717.py
@@ -0,0 +1,15 @@
+with open(r'..\file\24_21717.txt') as file:
+    data = file.readline()
+
+data = data.replace('RSQ', 'Rsq rsQ')
+data = data.split()
+ans = 10**10
+for i in range(len(data) - 129):
+    line = ''.join(data[i:i+129]).replace('sqrs', 'S')
+    cnt = 0
+    for j in data[i+129][3:]:
+        cnt += 1
+        if j not in 'Qq':
+            break
+    ans = min(ans,len(line)+cnt)
+print(ans)
