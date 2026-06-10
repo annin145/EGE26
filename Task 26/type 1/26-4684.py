0b4a97690ffecb903e71ac61efe73ebda758b9b6
annin145 <dementevaannet@gmail.com>
2026-06-10 09:38:50 +0200

    дз 1

--- Task 26/type 1/26-4684.py
+++ Task 26/type 1/26-4684.py
@@ -3,8 +3,12 @@ with open(r'..\files\26_4684.txt') as file:
     costs = [int(i) for i in file]
 
 cost = sorted(costs)
-price2 = sum(costs[:-(N//6)]) + sum(costs[-(N//6):])//2
-price1  = sum(costs) - sum(costs[::-1][5::6])//2
-
+price2 = sum(cost) - sum(cost[:N//6])//2
+price1  = sum(cost) - sum(cost[::-1][5::6])//2
+#
 print(price1,price2)
 
+price_1 = sum(cost) - sum(cost[:N//6+1])//2
+
+print(price_1)
+# print(cost)
