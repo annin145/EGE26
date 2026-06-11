a9e841b8b43909e2cfb9800d86b05b02c5613fc5
annindanx <dementevaannet@gmil.com>
2026-06-11 11:07:41 +0200

    дз 2

--- var-2-p/17.py
+++ var-2-p/17.py
@@ -5,7 +5,10 @@ ans = []
 max_num = max(i for i in data if i % 100 == 25)
 for i in range(len(data)-2):
     num1,num2,num3 = data[i:i+3]
-    if len(str(num1)) == 4 and len(str(num2)) == 4 and len(str(num3)) ==4:
+    u1 = len(str(abs(num1))) == 4
+    u2 = len(str(abs(num2))) == 4
+    u3 = len(str(abs(num3))) ==4
+    if  u1+u2+u3 <= 2:
         if num1+num2+num3 <= max_num:
             ans.append(num1+num2+num3)
 
